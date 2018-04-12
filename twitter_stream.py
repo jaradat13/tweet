22eb62bd2705bbbde37732565ea7c483c0ac88bc
jaradat13 <jaradat.musa@gmail.com>
2018-04-12 09:35:34 +0300

    main
    main

---
+++ twitter_stream.py
@@ -0,0 +1,28 @@
+﻿from keys import *
+
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+# consumer key, consumer secret, access token, access secret.
+
+
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        all_data = json.loads(data)
+        tweet = all_data['text']
+        print(tweet)
+        return (True)
+
+    def on_error(self, status):
+        print(status)
+
+
+
+auth = OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["keyword"])
